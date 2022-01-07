421bfc21b67407a6c23d260d86cb00d522844d12
Jil Shah <33742487+jilshah2001@users.noreply.github.com>
2022-01-07 17:06:14 -0500

    Update Main.c

--- Exercise/twoSum/Main.c
+++ Exercise/twoSum/Main.c
@@ -3,9 +3,9 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     *returnSize = 2;
-    int *ret = (int*)malloc(2*sizeof(int));
-    for (int i=0; i<=numsSize; i++){
-        for(int j=i+1;j<=numsSize; j++){
+    int *ret = (int*)malloc(2*sizeof(int)),i,j;
+    for (i=0; i<numsSize; i++){
+        for(j=i+1;j<numsSize; j++){
             if (target == nums[i]+nums[j]){
                 ret[0] = i;
                 ret[1] = j;
